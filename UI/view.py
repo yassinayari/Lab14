ab50769772ef4ea6a0ed3e8ae52c7f5557f76a5e
FranPi14 <123479714+FranPi14@users.noreply.github.com>
2025-05-31 16:25:11 +0200

    update view

--- UI/view.py
+++ UI/view.py
@@ -27,10 +27,11 @@ class View(ft.UserControl):
         self._ddStore = ft.Dropdown(label="Store")
         self._txtIntK = ft.TextField(label="Numero giorni massimo K")
         self._btnCreaGrafo = ft.ElevatedButton(text="Crea Grafo", on_click=self._controller.handleCreaGrafo)
-
         cont = ft.Container(self._ddStore, width=250, alignment=ft.alignment.top_left)
         row1 = ft.Row([cont, self._txtIntK, self._btnCreaGrafo], alignment=ft.MainAxisAlignment.CENTER,
                       vertical_alignment=ft.CrossAxisAlignment.END)
+
+
 
         self._btnCerca = ft.ElevatedButton(text="Cerca Percorso Massimo",
                                            on_click=self._controller.handleCerca)
@@ -40,8 +41,15 @@ class View(ft.UserControl):
         row2 = ft.Row([cont2, ft.Container(self._btnCerca, width=250)
         ], alignment=ft.MainAxisAlignment.CENTER)
 
+        self._btnRicorsione = ft.ElevatedButton(text="Ricorsione",
+                                           on_click=self._controller.handleRicorsione)
+
+        row3 = ft.Row([ft.Container(self._btnRicorsione, width=250)
+                       ], alignment=ft.MainAxisAlignment.CENTER)
+
         self._page.controls.append(row1)
         self._page.controls.append(row2)
+        self._page.controls.append(row3)
         self.txt_result = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
         self._page.controls.append(self.txt_result)
         self._page.update()
